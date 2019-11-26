19ffa9f1e8867d929cdf92043b9da6f1eaebf15c
ubuntu <gaidaumihai@gmai.com>
2019-11-26 16:20:16 +0200

    liqubase done

--- src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
-insert into users(id, name, email,disabled)
-values(1,'John','john@gmail.com', false);
-insert into users(id, name, email,disabled)
-values(2,'Rod','rod@gmail.com', false);
-insert into users(id, name, email,disabled)
-values(3,'Becky','becky@gmail.com', true);
